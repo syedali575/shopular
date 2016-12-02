cdb0ab75d46b3497938e10e73c971d4b007516b3
Syed Ali <sali3@cox.net>
2016-12-02 14:37:27 -0500

    Added more test to controller

--- test/specs/shop.controller.spec.js
+++ test/specs/shop.controller.spec.js
@@ -27,16 +27,22 @@
       ShopController = $controller("ShopController");
     }));
 
-    it("should add 1 + 1", function(){
-      var result = 1 + 1;
-      expect(result).to.equal(2);
-    });
 
     it("should have correct scope variables", function(){
       expect(ShopController.items).to.be.an("array");
       expect(ShopController.items.length).to.equal(1);
+      expect(typeof(ShopController.items[0].name)).to.be.a("string");
+      expect(ShopController.items[0].price).to.equal(100);
+      expect(ShopController.items[0].quantity).to.equal(1);
+      expect(typeof(ShopController.items[0].color)).to.be.a("string");
+
+
+
     });
 
+    it("should expect data object to have name:Boat", function(){
+
+    });
 
 
   });
